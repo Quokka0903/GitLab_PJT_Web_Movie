8fd93b3b0de2d859462e9c2b57a481a84283d614
dlgkssk38 <dlgkssk38@snu.ac.kr>
2022-11-16 13:58:42 +0900

    11/16 models까지 작성

--- final-pjt-back/movies/models.py
+++ final-pjt-back/movies/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=50)
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    release_date = models.DateField()
+    popularity = models.FloatField()
+    vote_average = models.FloatField()
+    overview = models.TextField()
+    poster_path = models.CharField(max_length=200)
+    genres = models.ManyToManyField(Genre)
+
+class Review(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    title = models.CharField(max_length=20)
+    content = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+# movies_liked 어떻게 할 지
+class MovieScore(models.Model):
+    score = models.IntegerField()
+    user = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="score")
+    movie = models.ManyToManyField(Movie, related_name="moviescore")
+
+class Cinema(models.Model):
+    pass
