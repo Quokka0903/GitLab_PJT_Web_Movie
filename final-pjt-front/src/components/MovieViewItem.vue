cc6ddbe6e1857609ef41441b70e3c967bfd87fe9
gs20wow@gmail.com <gs20wow@gmail.com>
2022-11-18 20:09:05 +0900

    메인페이지 실시간인기

---
+++ final-pjt-front/src/components/MovieViewItem.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="card mx-auto" style="width: 18rem">
+      <div class="img">
+        <img :src="jpg" class="card-img-top" height=100%>
+      </div>
+    <div class="card-body">
+      <h5 class="card-title">{{title}}</h5>
+    </div>
+  </div>
+  </template>
+  
+  <script>
+  export default {
+    name:'MovieCard',
+    props: {
+      movie : Object,
+    },
+    computed: {
+      title() {
+        return this.movie.title
+      },
+      jpg() {
+        return `https://image.tmdb.org/t/p/original/${this.movie.poster_path}`
+      },
+    }
+  }
+  </script>
+  
+  <style>
+  .card {
+    /* margin-top: auto; */
+    background-size: cover;
+  }
+
+  .img {
+    width : 270px;
+    height : 300px;
+  }
+  </style>
